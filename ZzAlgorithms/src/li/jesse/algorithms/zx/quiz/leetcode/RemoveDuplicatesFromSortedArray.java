d7ddd7264fe5ea74ccfeeeb934b77033be1b3020
Junying Li <jesselix@live.com>
2017-02-22 16:28:16 +0800

    update

---
+++ ZzAlgorithms/src/li/jesse/algorithms/zx/quiz/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,35 @@
+package li.jesse.algorithms.zx.quiz.leetcode;
+
+import org.junit.Test;
+
+/*
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. 
+It doesn't matter what you leave beyond the new length.
+*/
+
+public class RemoveDuplicatesFromSortedArray
+{
+	public int removeDuplicatesFromSortedArray(int[] nums)
+	{
+		return 1;
+	}
+	
+	@Test
+	public void testRemoveDuplicatesFromSortedArray()
+	{
+		int[] array = {0, 1, 3};
+		
+		removeDuplicatesFromSortedArray(array);
+	}
+}
+
+
+
+
