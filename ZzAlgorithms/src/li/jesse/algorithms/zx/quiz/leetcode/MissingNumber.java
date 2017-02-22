9a9217652cf325782306ae6dffd62b88bec7e019
Junying Li <jesselix@live.com>
2017-02-22 15:42:36 +0800

    update

---
+++ ZzAlgorithms/src/li/jesse/algorithms/zx/quiz/leetcode/MissingNumber.java
@@ -0,0 +1,41 @@
+package li.jesse.algorithms.zx.quiz.leetcode;
+
+import org.junit.Test;
+
+/*
+Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. Could you implement it using only constant extra space complexity?
+*/
+
+public class MissingNumber
+{
+	public int missingNumber(int[] nums)
+	{
+		for (int i = 0; i < nums.length; i++)
+		{
+			if (nums[i] != i)
+			{
+				return i;
+			}
+		}
+		
+		return 1;
+	}
+	
+	@Test
+	public void testMissingNumber()
+	{
+		int[] array = {0, 1, 3};
+		
+		System.out.println(missingNumber(array));
+	}
+}
+
+
+
+
--- ZzAlgorithms/src/li/jesse/algorithms/zx/quiz/leetcode/MoveZeroes.java
+++ ZzAlgorithms/src/li/jesse/algorithms/zx/quiz/leetcode/MoveZeroes.java
@@ -1,6 +1,47 @@
 package li.jesse.algorithms.zx.quiz.leetcode;
 
+import org.junit.Test;
+
+/*
+Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+Note:
+You must do this in-place without making a copy of the array.
+Minimize the total number of operations.
+*/
+
 public class MoveZeroes
 {
-
+	public void moveZeroes(int[] nums)
+	{
+		int temp, len = nums.length;
+		
+		for (int i = 0; i < len; i++)
+		{
+			for (int j = 0; j < len - 1 - i; j++)
+			{
+				if (nums[j] == 0 && nums[j + 1] != 0) 
+				{
+					temp = nums[j];
+					nums[j] = nums[j + 1];
+					nums[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	@Test
+	public void testMoveZeroes()
+	{
+		int[] array = {77, 0, 1, 0, 3, 12};
+		
+		moveZeroes(array);
+		
+		for (int i = 0; i < array.length; i++)
+		{
+			System.out.println(array[i]);
+		}
+	}
 }
