d416380251e5a9dee3829eee7191e118433529a8
Junying Li <jesselix@live.com>
2017-02-25 14:19:41 +0800

    update

---
+++ ZzAlgorithms/src/test/java/li/jesse/sorting/SortingTest.java
@@ -0,0 +1,20 @@
+package li.jesse.sorting;
+
+import org.junit.Test;
+
+public class SortingTest
+{
+	int[] number = {95, 45, 15, 78, 84, 51, 24, 12};
+	
+	@Test
+	public void testBubbleSort()
+	{
+//		number = BubbleSort.bubbleSort(number);
+//		for(int i = 0; i < number.length; i++)
+//		{
+//			System.out.print(number[i] + " ");
+//		}
+			
+		System.out.println();
+	}
+}
