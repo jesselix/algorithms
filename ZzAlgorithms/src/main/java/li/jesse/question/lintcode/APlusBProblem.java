b8f262ace6d6ac33a345538110f7cded38ee72e1
Junying Li <jesselix@live.com>
2017-05-12 13:25:05 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/lintcode/APlusBProblem.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/lintcode/APlusBProblem.java
@@ -1,4 +1,14 @@
 package li.jesse.question.lintcode;
 
-public class APlusBProblem {
+/*
+Write a function that add two numbers A and B. You should not use + or any arithmetic operators.
+
+a and b both are 32-bit integers.
+
+Example
+Given a=1 and b=2 return 3
+ */
+public class APlusBProblem
+{
+
 }
---
+++ ZzAlgorithms/src/main/java/li/jesse/question/lintcode/TrailingZeros.java
@@ -0,0 +1,12 @@
+package li.jesse.question.lintcode;
+
+/*
+Write an algorithm which computes the number of trailing zeros in n factorial.
+
+Example
+11! = 39916800, so the out should be 2
+ */
+public class TrailingZeros
+{
+
+}
