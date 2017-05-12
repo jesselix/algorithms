007be9ef709501022c55c1a58d4713ba5189ec23
Junying Li <jesselix@live.com>
2017-05-12 13:35:41 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/lintcode/APlusBProblem.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/lintcode/APlusBProblem.java
@@ -1,4 +1,15 @@
 package li.jesse.question.lintcode;
 
-public class APlusBProblem {
+/*
+Write a function that add two numbers A and B. You should not use + or any arithmetic operators.
+
+a and b are both 32-bit integers
+
+Example
+Given a=1 and b=2 return 3
+
+ */
+public class APlusBProblem
+{
+
 }
