8b19aa800b2bac9d63a98ea4f6d1603651fdeed4
Junying Li <jesselix@live.com>
2017-05-24 15:44:29 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/algorithm/sorting/QuickSort.java
+++ ZzAlgorithms/src/main/java/li/jesse/algorithm/sorting/QuickSort.java
@@ -2,5 +2,63 @@ package li.jesse.algorithm.sorting;
 
 public class QuickSort
 {
+    private int array[];
+    private int length;
 
+    public void myQuickSort(int[] inputArr)
+    {
+        if (inputArr == null || inputArr.length == 0)
+        {
+            return;
+        }
+        this.array = inputArr;
+        length = inputArr.length;
+        quickSort(0, length - 1);
+    }
+
+    public void quickSort( int lowerIndex, int higherIndex)
+    {
+        int i = lowerIndex;
+        int j = higherIndex;
+
+        int pivot = array[lowerIndex + (higherIndex - lowerIndex) / 2];
+
+        while (i <= j)
+        {
+            while (array[i] < pivot)
+            {
+                i++;
+            }
+            while (array[j] > pivot)
+            {
+                j--;
+            }
+            if (i <= j)
+            {
+                exchangeNumbers(i, j);
+                i++;
+                j--;
+            }
+        }
+
+        if (lowerIndex < j)
+        {
+            quickSort(lowerIndex, j);
+        }
+        if (i < higherIndex)
+        {
+            quickSort(i, higherIndex);
+        }
+    }
+
+    private void exchangeNumbers(int i, int j)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
+
+
+
+
--- ZzAlgorithms/src/test/java/li/jesse/test/algorithm/sorting/SortingTest.java
+++ ZzAlgorithms/src/test/java/li/jesse/test/algorithm/sorting/SortingTest.java
@@ -1,23 +1,40 @@
 package li.jesse.test.algorithm.sorting;
 
 import li.jesse.algorithm.sorting.BubbleSort;
+import li.jesse.algorithm.sorting.QuickSort;
 import org.junit.Test;
 
 public class SortingTest
 {
-    int[] number = {95, 45, 15, 78, 84, 51, 24, 12};
+    int[] numbers = {95, 45, 15, 78, 84, 51, 24, 12};
+    int length = numbers.length;
 
     @Test
     public void testBubbleSort()
     {
-        number = BubbleSort.bubbleSort(number);
+        numbers = BubbleSort.bubbleSort(numbers);
 
-        for(int i = 0; i < number.length; i++)
+        for(int i = 0; i < numbers.length; i++)
         {
-            System.out.print(number[i] + " ");
+            System.out.print(numbers[i] + " ");
         }
 
         System.out.println();
     }
 
+    @Test
+    public void testQuickSort()
+    {
+        QuickSort quickSort = new QuickSort();
+        quickSort.myQuickSort(numbers);
+        for (int i : numbers)
+        {
+            System.out.print(i + " ");
+        }
+    }
+
 }
+
+
+
+
