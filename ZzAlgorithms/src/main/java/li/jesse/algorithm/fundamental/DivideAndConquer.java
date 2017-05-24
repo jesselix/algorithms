8cdc6f5ffac7e8873105fe83a403f262aeb1fe07
Junying Li <jesselix@live.com>
2017-05-24 13:43:16 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/algorithm/fundamental/DivideAndConquer.java
+++ ZzAlgorithms/src/main/java/li/jesse/algorithm/fundamental/DivideAndConquer.java
@@ -1,6 +1,33 @@
 package li.jesse.algorithm.fundamental;
 
+/*
+Recursion is a "Programming Paradigm" which is generally used to implement the "Algorithmic Paradigm" Divide and Conquer.
+ */
 public class DivideAndConquer
 {
+    public static void hanoi(int level, char from, char inter, char to)
+    {
+        if (level == 1)
+        {
+            System.out.println("FROM " + from + " MOVE " + level + " TO " + to);
+        }
+        else
+        {
+            hanoi(level - 1, from, to, inter);
+            System.out.println("FROM " + from + " MOVE " + level + " TO " + to);
+            hanoi(level - 1, inter, from, to);
+        }
+    }
 
+    public static int fibonacci(int i)
+    {
+        if (i==1 || i == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return fibonacci(i - 1) + fibonacci(i - 2);
+        }
+    }
 }
---
+++ ZzAlgorithms/src/test/java/li/jesse/test/algorithm/fundamental/DivideAndConquerTest.java
@@ -0,0 +1,31 @@
+package li.jesse.test.algorithm.fundamental;
+
+import li.jesse.algorithm.fundamental.DivideAndConquer;
+import org.junit.Test;
+
+public class DivideAndConquerTest
+{
+    @Test
+    public void testHanoi()
+    {
+        DivideAndConquer.hanoi(3, 'A', 'B', 'C');
+    }
+
+    @Test
+    public void testFibonacci()
+    {
+        int j = 15;
+        System.out.println();
+        System.out.println("the first " + j + " fibonacci numbers are: ");
+        for (int i = 1; i <= j; i++)
+        {
+            System.out.print(DivideAndConquer.fibonacci(i) + "\t");
+
+            if (i % 5 == 0)
+            {
+                System.out.println();
+            }
+        }
+    }
+
+}
