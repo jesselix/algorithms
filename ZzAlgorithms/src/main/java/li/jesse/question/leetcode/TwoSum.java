372809d680d5f6e749cb60e658a411ee4de0dbbb
Junying Li <jesselix@live.com>
2017-06-05 16:13:23 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/leetcode/TwoSum.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/TwoSum.java
@@ -1,7 +1,7 @@
 package li.jesse.question.leetcode;
 
 /*
-#.1 Easy
+#.1 (Easy)
 
 Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
