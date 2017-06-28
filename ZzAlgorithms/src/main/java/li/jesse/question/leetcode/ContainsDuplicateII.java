1b3ec345541bbd2531ad66304aa27d4965e28dca
Junying Li <jesselix@live.com>
2017-06-28 17:10:00 +0800

    =

---
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/ContainsDuplicateII.java
@@ -0,0 +1,18 @@
+package li.jesse.question.leetcode;
+
+/*
+#219 Contains Duplicate II
+Easy
+
+Given an array of integers and an integer k,
+find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j]
+and the absolute difference between i and j is at most k.
+ */
+
+public class ContainsDuplicateII
+{
+    public static boolean containsDuplicateII(int[] nums, int k)
+    {
+        return true;
+    }
+}
