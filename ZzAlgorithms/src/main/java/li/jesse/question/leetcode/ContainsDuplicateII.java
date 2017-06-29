11239c4922f868fe320a6cf171596ba41b8a8672
Junying Li <jesselix@live.com>
2017-06-29 13:50:03 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/leetcode/ContainsDuplicateII.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/ContainsDuplicateII.java
@@ -13,6 +13,17 @@ public class ContainsDuplicateII
 {
     public static boolean containsDuplicateII(int[] nums, int k)
     {
-        return true;
+        for (int i = 0; i < nums.length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.length; j++)
+            {
+                if (nums[i] == nums[j] && Math.abs(i - j) <= k)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
