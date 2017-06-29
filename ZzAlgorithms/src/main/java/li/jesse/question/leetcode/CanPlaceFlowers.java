61c2fe20d5b870006698043ead1bb54615be98ff
Junying Li <jesselix@live.com>
2017-06-29 17:11:39 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/leetcode/CanPlaceFlowers.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/CanPlaceFlowers.java
@@ -1,6 +1,9 @@
 package li.jesse.question.leetcode;
 
 /*
+#605 Can Place Flowers
+Easy
+
 Suppose you have a long flowerbed in which some of the plots are planted and some are not.
 However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
 
