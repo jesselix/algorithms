cb14806da8d49d2f125c6a65f8d5f1f5aae29b7f
Junying Li <jesselix@live.com>
2017-07-18 16:13:57 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/lintcode/APlusBProblem.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/lintcode/APlusBProblem.java
@@ -2,7 +2,12 @@ package li.jesse.question.lintcode;
 
 /*
 Write a function that add two numbers A and B. You should not use + or any arithmetic operators.
-a and b are both 32-bit integers
+
+Clarification
+Are a and b both 32-bit integers?
+    Yes.
+Can I use bit operation?
+    Sure you can.
 
 Example
 Given a=1 and b=2 return 3
