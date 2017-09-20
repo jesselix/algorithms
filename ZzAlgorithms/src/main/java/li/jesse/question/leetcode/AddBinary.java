b13070f9bf73ca274cab775865cd023ee8a2d5e9
JESSE-WIN\jesse <jesselix@live.com>
2017-09-21 01:46:09 +0800

    =

---
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/AddBinary.java
@@ -0,0 +1,17 @@
+package li.jesse.question.leetcode;
+
+/*
+67. Add Binary
+Easy
+
+ Given two binary strings, return their sum (also a binary string).
+
+For example,
+a = "11"
+b = "1"
+Return "100".
+ */
+
+public class AddBinary
+{
+}
