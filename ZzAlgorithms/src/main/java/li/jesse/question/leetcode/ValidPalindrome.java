203acb0370f7fe3e892db31441cf7418ab3ec8f4
Junying Li <jesselix@live.com>
2017-09-25 16:40:05 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/leetcode/ValidPalindrome.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/ValidPalindrome.java
@@ -16,8 +16,9 @@ For the purpose of this problem, we define empty string as valid palindrome.
  */
 public class ValidPalindrome
 {
-    public static boolean isPalindrome(String s) {
-        if(s.length()==0)
+    public static boolean isPalindrome(String s)
+    {
+        if (s.length() == 0)
             return true;
 
         s = s.toUpperCase();
@@ -25,22 +26,26 @@ public class ValidPalindrome
         int low2 = '0', high2 = '9';
         int low = 0, high = s.length()-1;
 
-        while(low < high){
-            if((s.charAt(low)<low1||s.charAt(low)>high1)
-                    && (s.charAt(low)<low2||s.charAt(low)>high2)){
+        while (low < high)
+        {
+            if ((s.charAt(low) < low1 || s.charAt(low) > high1) && (s.charAt(low)<low2||s.charAt(low)>high2))
+            {
                 low++;
                 continue;
             }
 
-            if((s.charAt(high)<low1||s.charAt(high)>high1)
-                    && (s.charAt(high)<low2||s.charAt(high)>high2)){
+            if ((s.charAt(high) < low1 || s.charAt(high) > high1) && (s.charAt(high) < low2 || s.charAt(high) > high2))
+            {
                 high--;
                 continue;
             }
-            if(s.charAt(low) == s.charAt(high)){
+
+            if (s.charAt(low) == s.charAt(high))
+            {
                 low++;
                 high--;
-            }else
+            }
+            else
                 return false;
         }
         return true;
