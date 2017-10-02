80614ed240becf77aa9c63f36b935eba1cc3ee36
Junying Li <jesselix@live.com>
2017-10-02 21:13:54 +0800

    =

---
+++ ZzAlgorithms/src/test/java/li/jesse/question/leetcode/ReverseBitsTest.java
@@ -0,0 +1,12 @@
+package li.jesse.question.leetcode;
+
+import org.junit.Test;
+
+public class ReverseBitsTest
+{
+    @Test
+    public void testReverseBits()
+    {
+        System.out.println(ReverseBits.reverseBits(43261596));
+    }
+}
