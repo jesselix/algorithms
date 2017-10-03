b0403dcc20209a9cb81f3b5477ddf0b59b589057
Junying Li <jesselix@live.com>
2017-10-03 12:56:29 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/leetcode/PowerOfFour.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/PowerOfFour.java
@@ -16,7 +16,7 @@ public class PowerOfFour
 {
     public static boolean isPowerOfFour(int num)
     {
-        // 0x55555555 == 0101 0101 0101 0101 0101
+        // 0x55555555 == 0101 0101 0101 0101 0101 0101 0101 0101
         return (num & num - 1) == 0 && (num & 0x55555555) == num;
     }
 }
