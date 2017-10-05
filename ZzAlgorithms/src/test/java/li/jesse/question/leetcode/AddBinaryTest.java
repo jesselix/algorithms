6c59900f7457d20806321d9652b002f3b3287de9
Junying Li <jesselix@live.com>
2017-10-05 13:41:59 +0800

    =

--- ZzAlgorithms/src/test/java/li/jesse/question/leetcode/AddBinaryTest.java
+++ ZzAlgorithms/src/test/java/li/jesse/question/leetcode/AddBinaryTest.java
@@ -1,5 +1,15 @@
 package li.jesse.question.leetcode;
 
+import org.junit.Test;
+
 public class AddBinaryTest
 {
+    @Test
+    public void testAddBinary()
+    {
+        String a1 = "11";
+        String b1 = "1";
+
+        System.out.println(AddBinary.addBinary(a1, b1));
+    }
 }
---
+++ ZzAlgorithms/src/test/java/li/jesse/question/leetcode/FindTheDifferenceTest.java
@@ -0,0 +1,16 @@
+package li.jesse.question.leetcode;
+
+import org.junit.Test;
+
+public class FindTheDifferenceTest
+{
+    @Test
+    public void testFindTheDifference()
+    {
+        String x = "abcd";
+        String y = "abcBd";
+
+        System.out.println(FindTheDifference.findTheDifference(x, y));
+        System.out.println(FindTheDifference.findTheDifference2(x, y));
+    }
+}
