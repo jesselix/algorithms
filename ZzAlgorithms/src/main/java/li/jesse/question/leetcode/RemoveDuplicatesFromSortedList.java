b2c8cb8c6e7a71366c768262a20924bf6d8d6408
Junying Li <jesselix@live.com>
2017-10-14 11:26:43 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/leetcode/RemoveDuplicatesFromSortedList.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/RemoveDuplicatesFromSortedList.java
@@ -11,16 +11,16 @@ Given 1->1->2, return 1->2.
 Given 1->1->2->3->3, return 1->2->3.
  */
 
-public class RemoveDuplicatesFromSortedList
-{
-    public static ListNode deleteDuplicates(ListNode head)
-    {
+import li.jesse.datastructure.list.linkedlist.ListNode;
+
+public class RemoveDuplicatesFromSortedList {
+    public static ListNode deleteDuplicates(ListNode head) {
         if(head == null)
             return head;
         ListNode pre = head;
         ListNode cur = head.next;
-        while(cur!=null)
-        {
+
+        while(cur!=null) {
             if(cur.val == pre.val)
                 pre.next = cur.next;
             else
@@ -29,11 +29,4 @@ public class RemoveDuplicatesFromSortedList
         }
         return head;
     }
-}
-
-class ListNode
-{
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; }
 }
