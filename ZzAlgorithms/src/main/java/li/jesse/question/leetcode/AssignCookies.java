c63a2164e2b531fc825a0cad9749db1fb96873f9
Junying Li <jesselix@live.com>
2017-10-21 18:03:05 +0800

    =

---
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/AssignCookies.java
@@ -0,0 +1,28 @@
+package li.jesse.question.leetcode;
+
+import java.util.Arrays;
+
+/*
+
+ */
+public class AssignCookies {
+    public static int findContentChildren(int[] g, int[] s) {
+        int ret = 0;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0, j = 0;
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                ret++;
+                i++;
+                j++;
+            }else if (g[i] > s[j]) {
+                j++;
+            }
+        }
+
+        return ret;
+    }
+}
