01c6e56f6de43f16af1187c7085833e9d1116ae0
Junying Li <jesselix@live.com>
2017-10-31 16:30:45 +0800

    =

--- ZzAlgorithms/src/test/java/li/jesse/question/leetcode/IntersectionOfTwoLinkedListsTest.java
+++ ZzAlgorithms/src/test/java/li/jesse/question/leetcode/IntersectionOfTwoLinkedListsTest.java
@@ -9,20 +9,41 @@ public class IntersectionOfTwoLinkedListsTest {
 
     @Test
     public void testIntersectionOfTwoLinkedLists() {
-        ListNode node12 = new ListNode(12);
-        node12.setNext(null);
-        ListNode node11 = new ListNode(11);
-        node11.setNext(node12);
-        ListNode node10 = new ListNode(10);
-        node10.setNext(node11);
-        ListNode node2 = new ListNode(2);
-        node2.setNext(node10);
+
         ListNode node1 = new ListNode(1);
-        node1.setNext(node2);
-        ListNode node4 = new ListNode(4);
-        node4.setNext(node10);
+        ListNode node2 = new ListNode(2);
+
+        ListNode node10 = new ListNode(10);
+        ListNode node11 = new ListNode(11);
+        ListNode node12 = new ListNode(12);
+
         ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.setNext(node2);
+        node2.setNext(node10);
+        node10.setNext(node11);
+        node11.setNext(node12);
+        node12.setNext(null);
         node3.setNext(node4);
+        node4.setNext(node5);
+        node5.setNext(node10);
+
+//        ListNode node12 = new ListNode(12);
+//        node12.setNext(null);
+//        ListNode node11 = new ListNode(11);
+//        node11.setNext(node12);
+//        ListNode node10 = new ListNode(10);
+//        node10.setNext(node11);
+//        ListNode node2 = new ListNode(2);
+//        node2.setNext(node10);
+//        ListNode node1 = new ListNode(1);
+//        node1.setNext(node2);
+//        ListNode node4 = new ListNode(4);
+//        node4.setNext(node10);
+//        ListNode node3 = new ListNode(3);
+//        node3.setNext(node4);
 
         SinglyLinkedList linkedList1 = new SinglyLinkedList(node1);
         SinglyLinkedList linkedList2 = new SinglyLinkedList(node3);
