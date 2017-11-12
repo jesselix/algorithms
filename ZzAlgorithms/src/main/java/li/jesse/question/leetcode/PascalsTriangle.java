c35da8b90dfcbb9af6ef2937ef56c8de81e21022
Junying Li <jesselix@live.com>
2017-11-12 09:49:57 +0800

    =

--- ZzAlgorithms/src/main/java/li/jesse/question/leetcode/PascalsTriangle.java
+++ ZzAlgorithms/src/main/java/li/jesse/question/leetcode/PascalsTriangle.java
@@ -1,7 +1,10 @@
 package li.jesse.question.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
-#118 Pascal's Triangle
+118. Pascal's Triangle
 Easy
 
 Given numRows, generate the first numRows of Pascal's triangle.
@@ -16,7 +19,27 @@ Return
  [1,4,6,4,1]
 ]
  */
-
 public class PascalsTriangle
 {
+    public static ArrayList<ArrayList<Integer>> generate(int numRows) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
+        if(numRows<=0)
+            return res;
+        ArrayList<Integer> pre = new ArrayList<Integer>();
+        pre.add(1);
+        res.add(pre);
+        for(int i=2;i<=numRows;i++)
+        {
+            ArrayList<Integer> cur = new ArrayList<Integer>();
+            cur.add(1);
+            for(int j=0;j<pre.size()-1;j++)
+            {
+                cur.add(pre.get(j)+pre.get(j+1));
+            }
+            cur.add(1);
+            res.add(cur);
+            pre = cur;
+        }
+        return res;
+    }
 }
---
+++ ZzAlgorithms/src/test/java/li/jesse/question/leetcode/PascalsTriangleTest.java
@@ -0,0 +1,10 @@
+package li.jesse.question.leetcode;
+
+import org.junit.Test;
+
+public class PascalsTriangleTest {
+    @Test
+    public void testPascalsTriangle() {
+        System.out.println(PascalsTriangle.generate(5));
+    }
+}
