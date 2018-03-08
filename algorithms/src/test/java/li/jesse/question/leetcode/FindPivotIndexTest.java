71306bf64364f8212ea6208346f46abf25ca3a0f
Junying Li <jesselix@live.com>
2018-03-08 09:01:09 +0800

    =

---
+++ algorithms/src/test/java/li/jesse/question/leetcode/FindPivotIndexTest.java
@@ -0,0 +1,18 @@
+package li.jesse.question.leetcode;
+
+import org.junit.Test;
+
+public class FindPivotIndexTest {
+
+    @Test
+    public void testFindPivotIndex() {
+        int[] nums1 = {1, 7, 3, 6, 5, 6};
+        int[] nums2 = {1, 2, 3};
+        int[] nums3 = {2, 3};
+        int[] nums4 = {4};
+        System.out.println(FindPivotIndex.pivotIndex(nums1));
+        System.out.println(FindPivotIndex.pivotIndex(nums2));
+        System.out.println(FindPivotIndex.pivotIndex(nums3));
+        System.out.println(FindPivotIndex.pivotIndex(nums4));
+    }
+}
