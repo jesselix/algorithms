f35347f03ffd03d93ed962821fe4261ba193ca9f
Junying Li <jesselix@live.com>
2018-03-18 10:44:54 +0800

    =

--- zz_algorithms/src/main/java/li/jesse/searching/BinarySearch.java
+++ zz_algorithms/src/main/java/li/jesse/searching/BinarySearch.java
@@ -2,6 +2,19 @@ package li.jesse.searching;
 
 public class BinarySearch {
 
+    public enum Operation {
+        EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+        LESS,
+        LESS_EQUAL
+    }
+
+    public enum FirstLast {
+        FIRST,
+        LAST
+    }
+
     public static int binarySearch(int[] array, int key) {
         int left = 0;
         int right = array.length - 1;
@@ -20,47 +33,60 @@ public class BinarySearch {
         return -1;
     }
 
-    public static int findFirstElementEqualToKey(int[] array, int key) {
+
+    public static int findElement(int[] array, int key, FirstLast firstLast, Operation operation) {
         int left = 0;
         int right = array.length - 1;
         int mid;
 
-        while (left <= right) {
-            mid = (left + right) / 2;
-            if (key <= array[mid]) {
-                right = mid - 1;
-            } else {//arr[mid] < key
-                left = mid + 1;
-            }
-        }
-        //arr[right] < key <= arr[left]
-        //right是最后一个小于key的
-        //left是第一个大于等于key的
-        if (left < array.length && array[left] == key) {
-            return left;
-        }
-        return -1;
-    }
+        int case1 = 0;
+        boolean compareKeyWithArrayMid = false;
 
-    public static int findLastElementEqualToKey(int[] array, int key) {
-        int left = 0;
-        int right = array.length - 1;
-        int mid;
+        if (FirstLast.LAST.equals(firstLast) && Operation.LESS.equals(operation)) {
+            case1 = 0;
+        } else if (FirstLast.LAST.equals(firstLast) && Operation.LESS_EQUAL.equals(operation)) {
+            case1 = 1;
+        } else if (FirstLast.FIRST.equals(firstLast) && Operation.GREATER.equals(operation)) {
+            case1 = 2;
+        } else if (FirstLast.FIRST.equals(firstLast) && Operation.GREATER_EQUAL.equals(operation)) {
+            case1 = 3;
+        } else if (FirstLast.FIRST.equals(firstLast) && Operation.EQUAL.equals(operation)) {
+            case1 = 4;
+        } else if (FirstLast.LAST.equals(firstLast) && Operation.EQUAL.equals(operation)) {
+            case1 = 5;
+        }
 
         while (left <= right) {
             mid = (left + right) / 2;
-            if (key < array[mid]) {
+
+            if (case1 == 0 || case1 == 2 || case1 == 4) {
+                compareKeyWithArrayMid = compareKeyWithArrayMid = (key <= array[mid]);
+            } else {
+                compareKeyWithArrayMid = (key < array[mid]);
+            }
+
+            if (compareKeyWithArrayMid) {
                 right = mid - 1;
-            } else {//arr[mid] <= key
+            } else {
                 left = mid + 1;
             }
         }
-        //arr[right] <= key < arr[left]
-        //right是最后一个小于等于key的
-        //left是第一个大于key的
-        if (right >= 0 && array[right] == key) {
+
+        if (case1 == 0 || case1 == 2) {
             return right;
+        } else if (case1 == 1 || case1 == 3) {
+            return left;
+        } else if (case1 == 4) {
+            if (left < array.length && array[left] == key) {
+                return left;
+            }
+        } else if (case1 == 5) {
+            if (right >= 0 && array[right] == key) {
+                return right;
+            }
         }
+
         return -1;
     }
+
 }
--- zz_algorithms/src/test/java/li/jesse/searching/BinarySearchTest.java
+++ zz_algorithms/src/test/java/li/jesse/searching/BinarySearchTest.java
@@ -8,8 +8,15 @@ public class BinarySearchTest {
     public void testBinarySearch() {
         int[] array = {1, 2, 3, 4, 4, 4, 5};
 
-        System.out.println(BinarySearch.binarySearch(array, 4));
-        System.out.println(BinarySearch.findFirstElementEqualToKey(array, 4));
-        System.out.println(BinarySearch.findLastElementEqualToKey(array, 4));
+        System.out.println(BinarySearch.binarySearch(array, 1));
+//        System.out.println(BinarySearch.findFirstElementEqualToKey(array, 4));
+//        System.out.println(BinarySearch.findLastElementEqualToKey(array, 4));
+    }
+
+    @Test
+    public void testFindElement() {
+        int[] array = {1, 2, 3, 4, 4, 4, 5};
+
+        System.out.println(BinarySearch.findElement(array, 4, BinarySearch.FirstLast.LAST, BinarySearch.Operation.EQUAL));
     }
 }
