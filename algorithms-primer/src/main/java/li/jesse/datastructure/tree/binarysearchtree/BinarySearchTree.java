0181e57b5618ffeded7de74677a815836153963a
Junying Li <jesselix@live.com>
2018-05-09 15:55:04 +0800

    =

--- algorithms-primer/src/main/java/li/jesse/datastructure/tree/binarysearchtree/BinarySearchTree.java
+++ algorithms-primer/src/main/java/li/jesse/datastructure/tree/binarysearchtree/BinarySearchTree.java
@@ -1,5 +1,7 @@
 package li.jesse.datastructure.tree.binarysearchtree;
 
+import java.util.Queue;
+
 public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 
     private class Node {
@@ -8,45 +10,27 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         private Node left, right;
         private int size;
 
-    // --
+
     }
 
     private Node root;
 
+    // *********************** constructors ***********************
+
     public BinarySearchTree() {
-    }
-
-    public int size() {
-        return size(root);
-    }
-
-    private int size(Node node) {
-        if (node == null) {
-            return 0;
-        } else {
-            return node.size;
-        }
     }
 
 //    public boolean contains(Key key) {
 //
 //    }
 
-    private void preOrder(Node node) {
-        if (node != null) {
-            System.out.println(node.key);
-            preOrder(node.left);
-            preOrder(node.right);
-        }
-    }
 
-    public void preOrder() {
-        preOrder(root);
-    }
 
 //    private Node search(Node node, Key key) {
 //
 //    }
+
+    // ******************** getters & setters *********************
 
     public Value get(Key key) {
         return get(root, key);
@@ -68,4 +52,109 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
             return node.value;
         }
     }
+
+    // ************************ functions *************************
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) {
+            return 0;
+        } else {
+            return node.size;
+        }
+    }
+
+    public Key select(int k) {
+        if (k < 0 || k >= size()) {
+            throw new IllegalArgumentException("argument to select() is invalid: " + k);
+        }
+        Node x = select(root, k);
+        return x.key;
+    }
+
+    private Node select(Node x, int k) {
+        if (x == null) return null;
+        int t = size(x.left);
+        if      (t > k) return select(x.left,  k);
+        else if (t < k) return select(x.right, k-t-1);
+        else            return x;
+    }
+
+    public int rank(Key key) {
+        if (key == null) throw new IllegalArgumentException("argument to rank() is null");
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x) {
+        if (x == null) return 0;
+        int cmp = key.compareTo(x.key);
+        if      (cmp < 0) return rank(key, x.left);
+        else if (cmp > 0) return 1 + size(x.left) + rank(key, x.right);
+        else              return size(x.left);
+    }
+
+    private void keys(Node x, Queue<Key> queue, Key lo, Key hi) {
+        if (x == null) return;
+        int cmplo = lo.compareTo(x.key);
+        int cmphi = hi.compareTo(x.key);
+        if (cmplo < 0) keys(x.left, queue, lo, hi);
+//        if (cmplo <= 0 && cmphi >= 0) queue.enqueue(x.key);
+        if (cmphi > 0) keys(x.right, queue, lo, hi);
+    }
+
+    private boolean check() {
+        if (!isBST())
+            System.out.println("Not in symmetric order");
+        if (!isSizeConsistent())
+            System.out.println("Subtree counts not consistent");
+//        if (!isRankConsistent())
+//            System.out.println("Ranks not consistent");
+        return isBST() && isSizeConsistent();
+    }
+
+    private boolean isBST() {
+        return isBST(root, null, null);
+    }
+
+    private boolean isBST(Node x, Key min, Key max) {
+        if (x == null) return true;
+        if (min != null && x.key.compareTo(min) <= 0) return false;
+        if (max != null && x.key.compareTo(max) >= 0) return false;
+        return isBST(x.left, min, x.key) && isBST(x.right, x.key, max);
+    }
+
+    private boolean isSizeConsistent() { return isSizeConsistent(root); }
+    private boolean isSizeConsistent(Node x) {
+        if (x == null) return true;
+        if (x.size != size(x.left) + size(x.right) + 1) return false;
+        return isSizeConsistent(x.left) && isSizeConsistent(x.right);
+    }
+
+//    private boolean isRankConsistent() {
+//        for (int i = 0; i < size(); i++)
+//            if (i != rank(select(i))) return false;
+//        for (Key key : keys())
+//            if (key.compareTo(select(rank(key))) != 0) return false;
+//        return true;
+//    }
+
+    private void preOrder(Node node) {
+        if (node != null) {
+            System.out.println(node.key);
+            preOrder(node.left);
+            preOrder(node.right);
+        }
+    }
+
+    public void preOrder() {
+        preOrder(root);
+    }
+
 }
