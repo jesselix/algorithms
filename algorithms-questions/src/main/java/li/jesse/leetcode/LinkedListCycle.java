0c34790968919c704fd4c09b869c82ab02a9fb97
Junying Li <jesselix@live.com>
2018-05-10 16:25:37 +0800

    =

--- algorithms-questions/src/main/java/li/jesse/leetcode/LinkedListCycle.java
+++ algorithms-questions/src/main/java/li/jesse/leetcode/LinkedListCycle.java
@@ -1,4 +1,20 @@
 package li.jesse.leetcode;
 
+/*
+141. Linked List Cycle
+Easy
+
+ Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?
+ */
 public class LinkedListCycle {
+
+    public class linkedListCycle {
+//        public boolean hasCycle(ListNode head) {
+//
+//        }
+    }
+
 }
