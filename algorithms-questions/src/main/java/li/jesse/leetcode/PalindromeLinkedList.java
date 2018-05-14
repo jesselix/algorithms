a4f7a77da96071cc3b2fb20fa85ddd0ed49b330f
Junying Li <jesselix@live.com>
2018-05-14 16:25:51 +0800

    =

--- algorithms-questions/src/main/java/li/jesse/leetcode/PalindromeLinkedList.java
+++ algorithms-questions/src/main/java/li/jesse/leetcode/PalindromeLinkedList.java
@@ -2,6 +2,8 @@ package li.jesse.leetcode;
 
 import li.jesse.data.ListNode;
 
+import java.util.Stack;
+
 /*
 234. Palindrome Linked List
 Easy
@@ -22,6 +24,29 @@ Could you do it in O(n) time and O(1) space?
 public class PalindromeLinkedList {
 
     public boolean isPalindrome(ListNode head) {
-        return false;
+        if (head == null || head.next == null)
+            return true;
+        ListNode slow = head, fast = head;
+
+        Stack<Integer> s = new Stack<>();
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            s.push(slow.value);
+        }
+
+        if (fast.next != null)
+            s.pop();
+
+        while (slow.next != null) {
+            slow = slow.next;
+            Integer temp = s.peek();
+            s.pop();
+            if (temp != slow.value)
+                return false;
+        }
+
+        return true;
     }
 }
