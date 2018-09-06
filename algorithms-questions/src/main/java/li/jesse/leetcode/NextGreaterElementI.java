c1926246bbce7408979bfe864e0363b4cb1994a1
Junying Li <jesselix@live.com>
2018-09-06 14:08:44 +0800

    =

--- algorithms-questions/src/main/java/li/jesse/leetcode/NextGreaterElementI.java
+++ algorithms-questions/src/main/java/li/jesse/leetcode/NextGreaterElementI.java
@@ -30,6 +30,30 @@ public class NextGreaterElementI {
 
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        return null;
+        int output[] = new int[nums1.length];
+        int mark = 0;
+        boolean flag = false;
+
+        for (int i = 0; i < output.length; i++) {
+            output[i] = -1;
+        }
+
+        for (int i = 0; i < nums1.length; i++) {
+            flag = false;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    mark = nums1[i];
+                    System.out.println("ddd " + i + " " + j + " " + mark);
+                    flag = true;
+                    continue;
+                }
+                if (flag == true && nums2[j] > mark) {
+                    output[i] = nums2[j];
+                    break;
+                }
+            }
+        }
+
+        return output;
     }
 }
--- algorithms-questions/src/test/java/li/jesse/leetcode/NextGreaterElementITest.java
+++ algorithms-questions/src/test/java/li/jesse/leetcode/NextGreaterElementITest.java
@@ -6,6 +6,14 @@ public class NextGreaterElementITest {
 
     @Test
     public void testNextGreaterElementI() {
+        int input1[] = {4, 1, 2};
+        int input2[] = {1, 3, 4, 2};
+        int output[] = new int[input1.length];
 
+        output = NextGreaterElementI.nextGreaterElement(input1, input2);
+
+        for (int i = 0; i < output.length; i++) {
+            System.out.print(output[i] + " ");
+        }
     }
 }
