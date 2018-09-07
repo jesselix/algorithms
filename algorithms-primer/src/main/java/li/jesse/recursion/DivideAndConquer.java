5da86ce45e19d3df5f7abcfe6d5bf5906eb2feef
Junying Li <jesselix@live.com>
2018-09-07 15:50:45 +0800

    =

--- algorithms-primer/src/main/java/li/jesse/recursion/DivideAndConquer.java
+++ algorithms-primer/src/main/java/li/jesse/recursion/DivideAndConquer.java
@@ -3,30 +3,23 @@ package li.jesse.recursion;
 /*
 Recursion is a "Programming Paradigm" which is generally used to implement the "Algorithmic Paradigm" Divide and Conquer.
  */
-public class DivideAndConquer
-{
-    public static void hanoi(int level, char from, char inter, char to)
-    {
-        if (level == 1)
-        {
+public class DivideAndConquer {
+    public static void hanoi(int level, char from, char inter, char to) {
+        if (level == 1) {
             System.out.println("FROM " + from + " MOVE " + level + " TO " + to);
         }
-        else
-        {
+        else {
             hanoi(level - 1, from, to, inter);
             System.out.println("FROM " + from + " MOVE " + level + " TO " + to);
             hanoi(level - 1, inter, from, to);
         }
     }
 
-    public static int fibonacci(int i)
-    {
-        if (i==1 || i == 2)
-        {
+    public static int fibonacci(int i) {
+        if (i==1 || i == 2) {
             return 1;
         }
-        else
-        {
+        else {
             return fibonacci(i - 1) + fibonacci(i - 2);
         }
     }
--- algorithms-primer/src/test/java/li/jesse/recursion/DivideAndConquerTest.java
+++ algorithms-primer/src/test/java/li/jesse/recursion/DivideAndConquerTest.java
@@ -2,26 +2,23 @@ package li.jesse.recursion;
 
 import org.junit.Test;
 
-public class DivideAndConquerTest
-{
+public class DivideAndConquerTest {
+
     @Test
-    public void testHanoi()
-    {
+    public void testHanoi() {
         DivideAndConquer.hanoi(3, 'A', 'B', 'C');
     }
 
     @Test
-    public void testFibonacci()
-    {
+    public void testFibonacci() {
         int j = 15;
         System.out.println();
         System.out.println("the first " + j + " fibonacci numbers are: ");
-        for (int i = 1; i <= j; i++)
-        {
+
+        for (int i = 1; i <= j; i++) {
             System.out.print(DivideAndConquer.fibonacci(i) + "\t");
 
-            if (i % 5 == 0)
-            {
+            if (i % 5 == 0) {
                 System.out.println();
             }
         }
